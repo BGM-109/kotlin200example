59902ce3e55d417222ef6dcc1b10d65dea821324
“vividxxxxx” <vividxxxx@gmail.com>
2021-08-19 11:33:01 +0900

    Workout

---
+++ 064_inherites.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    
+    val person = Person("홍길동",35)
+    val student = Student("김길동",23,20202020)
+    println(person)
+    println(student)
+    
+}
+
+open class Person(val name:String, val age:Int)
+
+class Student(name:String, age:Int, val id: Int): Person(name,age)
---
+++ 065_Upcastring.kt
@@ -0,0 +1,9 @@
+
+fun main(args: Array<String>){
+    val person: Person = Student("John",32,20171218)
+    
+}
+
+open class Person(val name:String, val age:Int)
+
+class Student(name:String, age:Int, val id: Int): Person(name,age)
